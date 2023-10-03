bc53ddf597952bf2220004b58d7a835d1ae173af
PushpendraKPal <kumarpushp1992@gmail.com>
2023-10-03 19:08:37 +0530

    render the meals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Layout/Header";
+import MealList from "./components/Meals/MealList";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <MealList></MealList>
     </div>
   );
 }
