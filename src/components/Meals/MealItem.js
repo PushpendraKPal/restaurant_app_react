b1241c78125969eb3122b599e02878d87100b028
PushpendraKPal <kumarpushp1992@gmail.com>
2023-10-03 19:35:27 +0530

    added add amount button

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,11 +1,15 @@
+import IncreaseQty from "./IncreaseQty";
 import classes from "./Meal.module.css";
 
 const MealItem = ({ item }) => {
   return (
     <div className={classes.mealItem}>
-      <p className={`${classes.bold} ${classes.font20}`}>{item.foodName}</p>
-      <p>{item.aboutFood}</p>
-      <p className={classes.bold}>{`$${item.price}`}</p>
+      <div>
+        <p className={`${classes.bold} ${classes.font20}`}>{item.foodName}</p>
+        <p>{item.aboutFood}</p>
+        <p className={classes.bold}>{`$${item.price}`}</p>
+      </div>
+      <IncreaseQty></IncreaseQty>
     </div>
   );
 };
